1b4fc74af2726df694002938ae51cfe2b48a38d2
Kostas <karapis@digitalup.gr>
2024-06-12 17:58:47 +0300

    Frontend psiloallagi

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,25 @@
 import LoginLinks from '@/app/LoginLinks'
 
 export const metadata = {
-    title: 'Laravel',
+    title: 'Traveling Salesman',
 }
 
 const Home = () => {
     return (
         <>
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                <LoginLinks />
+            <div className="container mx-auto">
+                <div className='grid grid-cols-12 py-6'>
+                    <div className='col-span-6 order-1 xl:col-span-2 xl:order-1 px-3 my-3'>
+                        PROPERTIES
+                    </div>
+                    <div className='col-span-12 order-3 xl:col-span-8 xl:order-2 px-3 my-3'>
+                        MAP     
+                    </div>
+                    <div className='col-span-6 order-2 xl:col-span-2 xl:order-2 px-3 my-3'>
+                        ROUTE WAYPOINTS
+                    </div>
 
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                    <h1>TEST</h1>
-                </div>
-                       
+                </div>             
             </div>
         </>
     )
